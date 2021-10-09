eff75f4dcf17deed219816394b959f6f61517b12
zouzonghua <zouzonghua.cn@gmail.com>
2021-10-09 09:11:05 +0800

    feat: lualine add filename show

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -24,6 +24,11 @@ lualine.setup {
                 },
                 symbols = { added = '+', modified = '~', removed = '-' }, -- changes diff symbols
             },
+            {
+                'filename',
+                file_status = true, -- displays file status (readonly status, modified status)
+                path = 1, -- 0 = jst filename, 1 = relative path, 2 = absolute path
+            },
         },
         lualine_x = {
             {
