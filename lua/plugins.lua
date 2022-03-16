7eb6b847ea6fac0784e9cd576d5395a4f00752c5
zouzonghua <zouzonghua.cn@gmail.com>
2022-03-16 11:17:33 +0800

    chore: update autoinstall

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,49 +1,50 @@
 -- https://github.com/wbthomason/packer.nvim#bootstrapping
 
+vim.cmd 'packadd packer.nvim'
 
-local fn = vim.fn
+local present, packer = pcall(require, 'packer')
 
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+if not present then
+    local packer_path = vim.fn.stdpath 'data' .. '/site/pack/packer/opt/packer.nvim'
+
+    print 'Cloning packer..'
+    -- remove the dir before cloning
+    vim.fn.delete(packer_path, 'rf')
+    vim.fn.system {
+        'git',
+        'clone',
+        'https://github.com/wbthomason/packer.nvim',
+        '--depth',
+        '20',
+        packer_path,
+    }
+
+    vim.cmd 'packadd packer.nvim'
+    present, packer = pcall(require, 'packer')
+
+    if present then
+        print 'Packer cloned successfully.'
+    else
+        error('Couldn\'t clone packer !\nPacker path: ' .. packer_path .. '\n' .. packer)
+    end
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
-
--- Have packer use a popup window
 packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
+    display = {
+        open_fn = function()
+            return require('packer.util').float { border = 'single' }
+        end,
+        prompt_border = 'single',
+    },
+    git = {
+        clone_timeout = 6000, -- seconds
+    },
+    auto_clean = true,
+    compile_on_sync = true,
 }
 
 -- Install your plugins here
 return packer.startup(function(use)
-
     -- PluginManager
     use {
         'wbthomason/packer.nvim',
