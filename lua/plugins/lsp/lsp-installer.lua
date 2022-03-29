f0423b21d0b2b60d098b67a8f49c2a8b6940870b
zouzonghua <zouzonghua.cn@gmail.com>
2022-03-29 17:06:59 +0800

    refactor: update config

--- lua/plugins/lsp/lsp-installer.lua
+++ lua/plugins/lsp/lsp-installer.lua
@@ -1,4 +1,4 @@
--- need install `LspInstall stylelint_lsp sumneko_lua tsserver jsonls`
+-- need install `LspInstall stylelint_lsp sumneko_lua tsserver jsonls html`
 
 local status_ok, lsp_installer = pcall(require, 'nvim-lsp-installer')
 if not status_ok then
