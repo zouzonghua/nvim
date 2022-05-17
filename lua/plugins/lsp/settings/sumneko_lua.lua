9a029238a6486293c129f711fd6a774048026297
zouzonghua <zouzonghua.cn@gmail.com>
2022-05-17 09:05:04 +0800

    chore: update lua lsp config

--- lua/plugins/lsp/settings/sumneko_lua.lua
+++ lua/plugins/lsp/settings/sumneko_lua.lua
@@ -1,16 +1,24 @@
 return {
   settings = {
     Lua = {
-      autoFixOnSave = true,
-      autoFixOnFormat = true,
       diagnostics = {
+        neededFileStatus = {
+          ["codestyle-check"] = "Any",
+        },
         globals = { 'vim', 'hs' },
       },
-      workspace = {
-        library = {
-          [vim.fn.expand '$VIMRUNTIME/lua'] = true,
-          [vim.fn.stdpath 'config' .. '/lua'] = true,
+
+      format = {
+        enable = true,
+        defaultConfig = {
+          indent_style = 'space',
+          indent_size = '2',
         },
+      },
+
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
       },
     },
   },
