8f805b641e4c7bfe6e18630186cb784f3d4c2c59
zouzonghua <zouzonghua.cn@gmail.com>
2022-05-22 13:11:47 +0800

    refactor: close_buffer fn

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -31,24 +31,43 @@ function M.config()
     },
   }
 
-  _G.close_buffer = function()
-    if vim.fn.winnr() == vim.fn.winnr '$'
-        and vim.api.nvim_win_get_number(0) > 1
-    then
+  -- 1、当前有一个窗口 运行 :bd
+  -- 2、当前有两个窗口 运行 and 当前所在窗口不在 defx and 当前窗口包含 defx 运行 :defx bd defx wincmd l
+  -- 3、当前有两个窗口 运行 and 当前所在窗口不在 defx and 当前窗口不包含 defx 运行 :<C-w> q
+  -- 4、当前有三个窗口及以上 and 当前所在窗口不在 defx 运行 :<C-w> q
+  local close_buffer = function()
+    local total_buf_num = vim.fn.winnr '$'
+    local current_buf_num = vim.fn.winnr()
+    local is_last_buf = total_buf_num == current_buf_num
+    local is_defx_buf = vim.fn.getwinvar(current_buf_num, '&filetype') == 'defx'
+    local is_include_defx_buf = false
+
+    for i = current_buf_num, 1, -1 do
+      is_include_defx_buf = vim.fn.getwinvar(i, '&filetype') == 'defx'
+      if is_include_defx_buf then break end
+    end
+
+    if is_last_buf and current_buf_num == 1 then
+      vim.cmd 'bd'
+    end
+
+    if total_buf_num == 2 and is_include_defx_buf and not is_defx_buf then
       vim.cmd 'Defx -buffer-name=tab`tabpagenr()`'
       vim.cmd 'bd'
       vim.cmd 'Defx -buffer-name=tab`tabpagenr()`'
       vim.cmd 'wincmd l'
-    else
-      vim.cmd 'bd'
+    end
+
+    if total_buf_num == 2 and not is_include_defx_buf then
+      vim.cmd 'wincmd q'
+    end
+
+    if total_buf_num >= 3 and not is_defx_buf then
+      vim.cmd 'wincmd q'
     end
   end
 
-
-  local map = vim.api.nvim_set_keymap
-  local N = { noremap = true, silent = true }
-  map('n', '<C-c>', ':lua _G.close_buffer()<CR>', N)
+  vim.keymap.set('n', '<C-c>', close_buffer)
 end
 
 return M
-
