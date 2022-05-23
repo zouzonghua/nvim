71650b37c1a6b19ae8c24bd5b16ea4e26d97a121
zouzonghua <zouzonghua.cn@gmail.com>
2022-05-23 08:51:25 +0800

    style: format code

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,10 +1,10 @@
 -- command mode
-vim.keymap.set('c', '<C-a>', '<Home>')
 vim.keymap.set('c', '<C-e>', '<End>')
-vim.keymap.set('c', '<C-f>', '<Right>')
+vim.keymap.set('c', '<C-a>', '<Home>')
 vim.keymap.set('c', '<C-b>', '<Left>')
-vim.keymap.set('c', '<M-f>', '<S-Right>')
+vim.keymap.set('c', '<C-f>', '<Right>')
 vim.keymap.set('c', '<M-b>', '<S-Left>')
+vim.keymap.set('c', '<M-f>', '<S-Right>')
 
 -- move selected line
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
@@ -16,23 +16,23 @@ vim.keymap.set('n', '<Enter>', '<cmd>nohlsearch<Enter><Enter>')
 -- bufferline
 vim.keymap.set('n', '<C-l>', '<cmd>BufferLineCycleNext<CR>')
 vim.keymap.set('n', '<C-h>', '<cmd>BufferLineCyclePrev<CR>')
-vim.keymap.set('n', '<Leader>ll', '<cmd>BufferLineCloseRight<CR>')
 vim.keymap.set('n', '<Leader>hh', '<cmd>BufferLineCloseLeft<CR>')
+vim.keymap.set('n', '<Leader>ll', '<cmd>BufferLineCloseRight<CR>')
 vim.keymap.set('n', '<Leader>1', '<cmd>BufferLineGoToBuffer 1<CR>')
 vim.keymap.set('n', '<Leader>2', '<cmd>BufferLineGoToBuffer 2<CR>')
 vim.keymap.set('n', '<Leader>3', '<cmd>BufferLineGoToBuffer 3<CR>')
 vim.keymap.set('n', '<Leader>4', '<cmd>BufferLineGoToBuffer 4<CR>')
 
 -- neoterm
+vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
 vim.keymap.set('n', '<C-y>', '<cmd>Ttoggle<CR>')
 vim.keymap.set('t', '<C-y>', '<C-\\><C-N>:Ttoggle<CR>')
-vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
 
 -- telescope
 local telescope = require("telescope.builtin")
-vim.keymap.set('n', '<Leader>ff', telescope.find_files)
 vim.keymap.set('n', '<Leader>fg', telescope.live_grep)
 vim.keymap.set('n', '<Leader>fh', telescope.help_tags)
+vim.keymap.set('n', '<Leader>ff', telescope.find_files)
 
 -- defx
 vim.keymap.set('n', '<C-e>', '<cmd> Defx -buffer-name=tab`tabpagenr()`<CR>')
